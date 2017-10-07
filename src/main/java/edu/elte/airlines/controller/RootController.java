39d9187ebce03a7dcd0f894d5e90d84b33b94a15
David Petz <petzdavid@gmail.com>
2017-10-07 13:01:48 +0200

    Added: Logout

--- src/main/java/edu/elte/airlines/controller/RootController.java
+++ src/main/java/edu/elte/airlines/controller/RootController.java
@@ -2,10 +2,14 @@ package edu.elte.airlines.controller;
 
 import edu.elte.airlines.util.webcontext.WebContextInitializer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -25,7 +29,7 @@ public class RootController {
 
 	private final String templateName = "login";
 
-	@RequestMapping("/")
+	@RequestMapping(value = {"/", "/login"})
 	public void getLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		WebContext ctx = WebContextInitializer.createContext(request, response);
 		templateEngine.process(templateName, ctx, response.getWriter());
@@ -36,7 +40,16 @@ public class RootController {
 		UserDetails userDetails = userDetailsService.loadUserByUsername(userName);
 		if(userDetails != null) {
 			WebContext ctx = WebContextInitializer.createContext(request, response);
+			ctx.setVariable("username", userDetails.getUsername());
 			templateEngine.process("index", ctx, response.getWriter());
 		}
+	}
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public void logoutPage (HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (auth != null){
+			new SecurityContextLogoutHandler().logout(request, response, auth);
+		}
+		getLogin(request, response);
 	}
 }

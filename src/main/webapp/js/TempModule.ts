38a2d5e7fd3025b615f43079bf011fef1fa04a19
David Petz <petzdavid@gmail.com>
2017-10-27 18:28:10 +0200

    Some minor refactor

---
+++ src/main/webapp/js/TempModule.ts
@@ -0,0 +1,9 @@
+declare var angular: any;
+
+angular.module('demo', [])
+    .controller('Hello', function($scope, $http) {
+        $http.get('http://rest-service.guides.spring.io/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+    });
